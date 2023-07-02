b00306d8ae71fb2f513bf36d6fa8ff706843152c
René Baumgärtner <terminator1000000@web.de>
2023-07-02 13:34:19 +0200

    build(common): prepare cypress tests and update cypress #23 #22

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     screenshotsFolder: 'tests/e2e/screenshots',
     videosFolder: 'tests/e2e/videos',
     e2e: {
+        baseUrl: "http://localhost:8080",
         // We've imported your old cypress plugins here.
         // You may want to clean this up later by importing these.
         setupNodeEvents(on, config) {
@@ -12,6 +13,8 @@ export default defineConfig({
             return require('./tests/e2e/plugins/index.js')(on, config);
         },
         specPattern: 'tests/e2e/specs/**/*.cy.{js,jsx,ts,tsx}',
-        supportFile: 'tests/e2e/support/index.js'
+        supportFile: 'tests/e2e/support/index.js',
+        watchForFileChanges: false,
+        video: false
     }
 });
